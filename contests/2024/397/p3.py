9217f2e2894cf32bb87f45c8d796d86480e97ce8
arslantu <arslantu@arslantu.xyz>
2024-05-14 05:52:17 +0800

    complete 3148

--- contests/2024/397/p3.py
+++ contests/2024/397/p3.py
@@ -9,16 +9,20 @@ class Solution:
 
     def maxScore(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        dp = [[-inf for _ in range(n)] for _ in range(m)]
-
-        max_val_pos = [[(-1, -1) for _ in range(n)] for _ in range(m)]
-
+        ans = -inf
+        max_values = [[-inf for _ in range(n)] for _ in range(m)]
         for j in range(n - 1, -1, -1):
             for i in range(m - 1, -1, -1):
-                if i < m - 1 and n < n -1:
-                    
-
-        return max([max(row) for row in dp])
+                if i < m - 1:
+                    max_values[i][j] = max(
+                        max_values[i][j], grid[i + 1][j], max_values[i + 1][j]
+                    )
+                if j < n - 1:
+                    max_values[i][j] = max(
+                        max_values[i][j], grid[i][j + 1], max_values[i][j + 1]
+                    )
+                ans = max(ans, max_values[i][j] - grid[i][j])
+        return ans
 
 
 def test():
@@ -27,8 +31,8 @@ def test():
     """
     sl = Solution()
     examples = [
-        # ([[9, 5, 7, 3], [8, 9, 6, 1], [6, 7, 14, 3], [2, 5, 3, 1]], 9),
-        # ([[4, 3, 2], [3, 2, 1]], -1),
+        ([[9, 5, 7, 3], [8, 9, 6, 1], [6, 7, 14, 3], [2, 5, 3, 1]], 9),
+        ([[4, 3, 2], [3, 2, 1]], -1),
         (
             [
                 [6, 1, 4, 6], [5, 6, 3, 5], [5, 3, 10, 3], [8, 7, 1, 5],
